ccaf4c958047e8eacebe494329ee55242221ce85
OneKek <iliaberezovsky227@gmai.com>
2023-09-11 16:17:43 +0200

    11

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,5 +1,5 @@
 // import { styles } from "../styles/styles";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../index.css";
 
 const Header = () => {
